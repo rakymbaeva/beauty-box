61c96c6a15f200b5129265e3d689e72a632c1ec7
Aiana <rakymbaevaaiana@gmail.com>
2020-05-18 10:42:55 +0600

    drawer stylize

--- src/components/BeautyBox/BeautyKit/Beauty/Beauty.js
+++ src/components/BeautyBox/BeautyKit/Beauty/Beauty.js
@@ -4,5 +4,8 @@ import classes from "./Beauty.module.css";
 export default ({ type }) => {
   const beautyClasses = [classes.Beauty, classes[type]];
 
+
+  
+
   return <div className={beautyClasses.join(" ")}></div>;
 };
